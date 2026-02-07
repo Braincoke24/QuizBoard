3647fd9368fcac6acd0d7286c752dfb0c065b858
Konrad Kleineidam <kkonradr@students.uni-mainz.de>
2026-02-08 00:25:07 +0900

    remove done todos

--- src/main.ts
+++ src/main.ts
@@ -30,8 +30,4 @@ main()
 // TODO: write tests for BuzzerConfig
 // TODO: add settings/options wheel
 // TODO: add option to restart game
-// TODO: add game version
-// TODO: add welcome screen
 // TODO: add option to go back to welcome screen
-
-// NOTE: Welcome screen should have version number, big title and author remark and SetupGame Button (later: add demo button for game with dummy players)
